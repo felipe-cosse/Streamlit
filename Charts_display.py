e6187eb0a6e278c53a054cd813a4228584c94830
Felipe Cosse Batista <felipe.cosse@a3data.com.br>
2019-11-01 09:35:51 -0300

    Alter Charts Display

--- Charts_display.py
+++ Charts_display.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import streamlit as st
 import plotly.figure_factory as ff
 from bokeh.plotting import figure
+import graphviz as graphviz
 
 arr = np.random.normal(1, 1, size=100)
 plt.hist(arr, bins=20)
@@ -47,7 +49,70 @@ df = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.header('')
-st.deck_gl_chart(layers = [{
-        'data': df,
-        'type': 'ScatterplotLayer'
-    }])
+st.deck_gl_chart(
+     viewport={
+         'latitude': 37.76,
+         'longitude': -122.4,
+         'zoom': 11,
+         'pitch': 50,
+     },
+     layers=[{
+         'type': 'HexagonLayer',
+         'data': df,
+         'radius': 200,
+         'elevationScale': 4,
+         'elevationRange': [0, 1000],
+         'pickable': True,
+         'extruded': True,
+     }, {
+         'type': 'ScatterplotLayer',
+         'data': df,
+     }])
+
+# Create a graphlib graph object
+st.header('Graphviz Chart')
+
+st.graphviz_chart('''
+    digraph {
+        run -> intr
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''')
+
+chart_data_line = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.header('Line Chart')
+st.line_chart(chart_data_line)
+
+chart_data_area = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.header('Area Chart')
+st.area_chart(chart_data_area)
+
+
+chart_data_bar = pd.DataFrame(
+     [[20, 30, 50]],
+     columns=['a', 'b', 'c'])
+st.header('Bar Chart')
+st.bar_chart(chart_data_bar)
+
+
+df = pd.DataFrame(
+     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     columns=['lat', 'lon'])
+st.header('Map')
+st.map(df)
